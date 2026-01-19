e52c7426b03c7e1a482f5f6aac6ea5518fc6534f
PRABHAHARAN A <122508061+PRABHAHARAN-2020@users.noreply.github.com>
2026-01-19 04:41:16 +0000

    cmt

--- program3.py
+++ program3.py
@@ -1,1 +1,38 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
+
+class Shape:
+    def __init__(self, points):
+        self.points = points
+
+    def display(self):
+        for i in self.points:
+            print(i.x, i.y)
+
+    def move_shape(self, dx, dy):
+        for i in self.points:
+            i.move(dx, dy)
+
+# Creating Point objects
+p1 = Point(1, 2)
+p2 = Point(4, 2)
+p3 = Point(3, 5)
+
+# Creating Shape object
+s1 = Shape([p1, p2, p3])
+
+print("Shape: Triangle")
+print("Before moving:")
+s1.display()
+
+s1.move_shape(5, 5)
+
+print("After moving:")
+s1.display()
 
